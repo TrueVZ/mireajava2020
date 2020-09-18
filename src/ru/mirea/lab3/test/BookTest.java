1c9efa000380780f4b0343b0715b047f29fc9410
TrueVZ <zaikinvala59@gmail.com>
2020-09-18 21:04:57 +0300

    fix lab4

--- src/ru/mirea/lab3/test/BookTest.java
+++ src/ru/mirea/lab3/test/BookTest.java
@@ -1,13 +1,27 @@
 package ru.mirea.lab3.test;
 
+import org.junit.Test;
 import ru.mirea.lab3.Book;
+import static org.junit.Assert.*;
 
 public class BookTest {
-    public static void main(String[] args) {
-        Book b = new Book("Толстой","Война и мир", 1867);
-        System.out.println(b.getAuthor());
-        System.out.println(b.getTitle());
-        System.out.println(b.getYear());
-        System.out.println(b);
+
+    @Test
+    public void getYear() {
+        Book book = new Book("Ivan", "MIr", 1999);
+        assertEquals(1999, book.getYear());
+
+    }
+
+    @Test
+    public void getAuthor() {
+        Book book = new Book("Ivan", "MIr", 1999);
+        assertEquals("Ivan", book.getAuthor());
+    }
+
+    @Test
+    public void getTitle() {
+        Book book = new Book("Ivan", "Mir", 1999);
+        assertEquals("Mir", book.getTitle());
     }
 }
--- src/ru/mirea/lab3/test/CircleTest.java
+++ src/ru/mirea/lab3/test/CircleTest.java
@@ -1,12 +1,27 @@
 package ru.mirea.lab3.test;
 
+import org.junit.Test;
 import ru.mirea.lab3.Circle;
 
+import static org.junit.Assert.*;
+
 public class CircleTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void getRadius() {
         Circle c = new Circle(10, "Blue");
-        System.out.println(c.getColor());
-        System.out.println(c.getRadius());
-        System.out.println(c);
+        assertEquals(10, c.getRadius(), 0.1);
+    }
+
+    @Test
+    public void getColor() {
+        Circle c = new Circle(10, "Blue");
+        assertEquals("Blue", c.getColor());
+    }
+
+    @Test
+    public void getArea() {
+        Circle c = new Circle(1, "Red");
+        assertEquals(9.85, c.getArea(), 0.01);
     }
 }
