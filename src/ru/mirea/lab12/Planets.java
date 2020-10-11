a6f182b2aef3a977c4e9f2a5c17b6eb31b33ae89
TrueVZ <zaikinvala59@gmail.com>
2020-10-12 00:29:10 +0300

    add lab12

--- src/ru/mirea/lab12/Planets.java
+++ src/ru/mirea/lab12/Planets.java
@@ -1,30 +1,43 @@
 package ru.mirea.lab12;
 
 public enum Planets {
-    Mercury ("Меркурий", 11111, 4900),
-    Venus ("Венера", 1111, 12100),
-    Earth("Земля", 1111, 12760),
-    Mars("Марс", 1111, 6800),
-    Jupiter("Юпитер", 1111, 143000),
-    Saturn("Сатурн", 1111, 120600),
-    Uranus("Уран", 1111, 51000),
-    Neptune("Нептун", 1111, 50000);
-
+    Mercury ("Меркурий", 3.301e+23, 2440),
+    Venus ("Венера", 	4.867e+24, 6052),
+    Earth("Земля", 5.972e+24, 6378),
+    Mars("Марс", 6.417e+23, 3397),
+    Jupiter("Юпитер", 1.899e+27, 71490),
+    Saturn("Сатурн", 5.685e+26, 60270),
+    Uranus("Уран", 8.682e+25, 25560),
+    Neptune("Нептун", 	1.024e+26, 24760);
 
     private String name;
-    private int massa;
-    private int radius;
+    private double massa;
+    private double radius;
+    public static final double G = 6.67;
+
+
+    double gravity() {
+        return G * (massa / (radius * radius));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMassa() {
+        return massa;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
 
     @Override
     public String toString() {
-        return "Planets{" +
-                "name='" + name + '\'' +
-                ", massa=" + massa +
-                ", radius=" + radius +
-                '}';
+        return name;
     }
 
-    Planets(String text, int massa, int radius) {
+    Planets(String text, double massa, double radius) {
         this.name = text;
         this.massa = massa;
         this.radius = radius;
